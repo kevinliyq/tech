cfb22cf45e84532cf9285ede6f252cff7ac297d4
kevin <kevinliyq@qq.com>
2019-09-28 22:56:40 +0800

    a new way to find annotation

--- tech-netty/src/main/java/com/study/liyq/technetty/SpringNettyApplication.java
+++ tech-netty/src/main/java/com/study/liyq/technetty/SpringNettyApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
+//another way to find annotation beyonds spring scan
+//https://marcin-chwedczuk.github.io/creating-and-using-adnotations-in-java
 @SpringBootApplication
 @ComponentScan(includeFilters = @ComponentScan.Filter(HttpHandler.class))
 public class SpringNettyApplication {
