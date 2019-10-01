08af7047a563a897133c823362ab228f991472b5
yoli <yoli@tripadvisor.com>
2019-10-01 22:17:05 +0800

    create tech-memcached

---
+++ tech-memcached/src/main/java/com/study/liyq/techmemcached/TechMemcachedApplication.java
@@ -0,0 +1,13 @@
+package com.study.liyq.techmemcached;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechMemcachedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechMemcachedApplication.class, args);
+	}
+
+}
